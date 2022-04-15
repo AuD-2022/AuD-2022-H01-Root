a5d91944f12c2db9644993f576baa141019dee9e
Dustin Glaser <dustin.glaser@posteo.de>
2022-04-15 15:37:33 +0200

    fix imports

--- src/test/java/h01/ListTest.java
+++ src/test/java/h01/ListTest.java
@@ -1,7 +1,8 @@
 package h01;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ListTest {
 
